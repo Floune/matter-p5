9715abbbed06cfdcd6f414db81f2426f235e8f17
floune <rfournier@simplonprod.co>
2021-04-18 11:27:03 +0200

    toggle draw / mouseConstraint

--- js/utils/interactions.js
+++ js/utils/interactions.js
@@ -4,7 +4,6 @@ function enableMouse() {
   mConstraint = MouseConstraint.create(engine, {
     mouse: canvasmouse
   })
-  //World.add(world, mConstraint)
 }
 
 function mousePressed() {
@@ -21,25 +20,32 @@ function keyPressed() {
  console.log(keyCode)
   let ki = keyCode
   switch(ki) {
+    // space
     case 32:
       balls.push(new Ball(mouseX, mouseY));
       break;
+    // u
     case 85:
       let toDelete = lines.pop();
       World.remove(world, toDelete.body);
       break;
+    // f1
     case 112:
       legend = !legend
       break;
+    // r
     case 82:
       resetSketch()
       break;
+    // b
     case 66:
       resetBalls()
       break;
+    // d
     case 68:
       toggleDraw()
       break;
+    // l
     case 76:
       resetLines()
       break;
@@ -49,10 +55,17 @@ function keyPressed() {
 
 function toggleDraw() {
   drawmode = !drawmode
+  if (drawmode == false) {
+    World.add(world, mConstraint)
+  } else {
+    World.remove(world, mConstraint)
+  }
 }
 
 function mouseReleased() {
-  createLine()
+  if (drawmode === true) {
+    createLine()
+  }
 }
 
 function handleLoopInteractions() {
