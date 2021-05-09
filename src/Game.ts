2382be898226ec89ebad45cc520269c72d18064b
Callan <cerveauxallanjean@gmail.com>
2021-05-09 13:27:08 +0200

    Can paint now !

--- src/Game.ts
+++ src/Game.ts
@@ -1,6 +1,6 @@
 import { AddToDom } from './dom';
 import { CanvasRender } from './renderer';
-import {Mouse} from "./utils/mouse";
+import { Mouse } from './utils/Mouse';
 
 export class Game {
   readonly version: string = '0.0.1-Alpha-Bravo-Charlie-Tango';
@@ -10,12 +10,11 @@ export class Game {
   mouse: Mouse;
   painting: boolean = false;
 
-  contructor() {
-
-  }
+  contructor() {}
 
   boot(): void {
     const render = new CanvasRender();
+    let time = performance.now();
 
     this.render = render;
 
@@ -25,26 +24,31 @@ export class Game {
 
     this.context = this.render.ctx;
 
-    this.mouse = new Mouse()
-    this.mouse.getClick();
-    this.listeners();
-    this.step()
+    this.mouse = new Mouse();
+    this.mouse.on('mousemove');
+    this.mouse.on('click');
+    this.step(time);
   }
 
-  step(): void {
-    // Keep requesting new frames
-    window.requestAnimationFrame(() => this.step())
+  step = (t?: number): void => {
+    window.requestAnimationFrame(this.step);
 
+    console.log('pos :', this.mouse.position, 'mousedown :', this.mouse.mouseDown);
     this.draw();
-  }
+  };
 
   draw(): void {
     //this.reset()
+    if (!this.painting) {
+      this.context.beginPath();
+      this.painting = true;
+    }
 
-    if (this.mouse._mouseDown) {
-      console.log(this.mouse.xPos, this.mouse.yPos)
+    if (this.mouse._mouseDown && this.painting) {
       this.context.lineTo(this.mouse.xPos, this.mouse.yPos);
       this.context.stroke();
+    } else {
+      this.painting = false;
     }
   }
 
@@ -53,12 +57,10 @@ export class Game {
   }
 
   listeners(): void {
-    if(this.mouse._mouseDown){
-      this.context.beginPath()
-      this.context.moveTo(this.mouse.xPos, this.mouse.yPos)
-    }else {
-
-      this.context.closePath()
+    if (this.mouse._mouseDown) {
+      this.context.moveTo(this.mouse.xPos, this.mouse.yPos);
+    } else {
+      this.context.closePath();
     }
   }
 }
