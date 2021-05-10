c86ed432779a2423cc1dff2ab762c078396b69e3
Callan <cerveauxallanjean@gmail.com>
2021-05-10 12:54:59 +0200

    remove draw function

--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,7 @@
 import { AddToDom } from './dom';
 import { Camera } from './camera/Camera';
 import { CanvasRender } from './renderer';
-import { Mouse } from './utils/Mouse';
+import { Mouse } from './mouse/Mouse';
 
 export class Game {
   readonly version: string = '0.0.1-Alpha-Bravo-Charlie-Tango';
@@ -33,8 +33,6 @@ export class Game {
     // this.context.scale(1.5, 1.5);
 
     this.mouse = new Mouse();
-    this.mouse.on('mousemove');
-    this.mouse.on('wheel');
     this.camera = new Camera();
 
     this.step(time);
@@ -42,42 +40,7 @@ export class Game {
 
   step = (t?: number): void => {
     window.requestAnimationFrame(this.step);
-
-    this.draw();
   };
-
-  draw(): void {
-    this.reset();
-    this.context.save();
-
-    if (this.mouse.deltaY > 0) {
-      this.camera.zoomIn();
-      this.mouse.deltaY = 0;
-    } else if (this.mouse.deltaY < 0) {
-      this.camera.zoomOut();
-      this.mouse.deltaY = 0;
-    }
-
-    if (this.mouse.mouseDown) {
-      this.camera.translate(this.mouse.x, this.mouse.y);
-    }
-    this.camera.dragOffset(this.mouse.x, this.mouse.y);
-    this.context.translate(this.camera.translatePos.x, this.camera.translatePos.y);
-
-    this.context.strokeRect(200, 200, 200, 500);
-
-    // if (!this.painting) {
-    //   this.context.beginPath();
-    //   this.painting = true;
-    // }
-    // if (this.mouse.mouseDown && this.painting) {
-    //   this.context.lineTo(this.mouse.x, this.mouse.y);
-    //   this.context.stroke();
-    // } else {
-    //   this.painting = false;
-    // }
-    this.context.restore();
-  }
 
   reset(): void {
     this.context.clearRect(0, 0, this.render.width, this.render.height);
