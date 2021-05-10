95d809f7c16454a9d2887c447527bd6e63f5d85b
Callan <cerveauxallanjean@gmail.com>
2021-05-10 12:38:08 +0200

    Export all from index file

---
+++ src/mouse/index.ts
@@ -0,0 +1,4 @@
+import { Mouse } from './Mouse';
+import { MouseAbstract } from './mouse.abstract';
+import { MouseInterface } from './mouse.interface';
+export { MouseAbstract, MouseInterface, Mouse };
